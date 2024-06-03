7b7e928c3918d130d2f9007b9a61d499a865af32
tnaveen-kumar <123799811+tnaveen-kumar@users.noreply.github.com>
2024-06-03 12:59:10 +0530

    Update 03-ec2.tf

--- 03-ec2.tf
+++ 03-ec2.tf
@@ -4,26 +4,14 @@ resource "aws_instance" "myec2-1" {
     availability_zone = var.Az_location
     key_name = var.kp
     subnet_id = aws_subnet.Dev_Public_Subnet.id
-    #security_groups = [aws_security_group.public_sg.name]
+    vpc_security_group_ids = [aws_security_group.public_sg.id]
     user_data = file("install_httpd.sh")
     tags = {
     Name = "${var.env_code}-VM-Public"
   }
 }
 
-/*
-resource "aws_instance" "myec2-2" {
-    ami = var.ami_id
-    instance_type = var.Instance_type
-    availability_zone = var.Az_location
-    key_name = var.kp
-    subnet_id = aws_subnet.Dev_Private_Subnet.id
-    tags = {
-    Name = "${var.env_code}-VM-Private"
-  }
-}
-*/
 
 output "Site-Url" {
-  value = aws_instance.myec2-1.public_ip
+  value = "http://${aws_instance.myec2-1.public_ip}"
 }
